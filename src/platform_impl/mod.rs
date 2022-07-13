7b07798a1cd517ee8b64675269ab3ec286c82621
Jason Tsai <jason@pews.dev>
2022-07-13 22:07:06 +0800

    chore: move macos impl under macos folder

--- src/platform_impl/mod.rs
+++ src/platform_impl/mod.rs
@@ -1,24 +1,5 @@
-mod global_shortcut;
-mod keycode;
+pub use self::platform::*;
 
-pub use self::{
-  global_shortcut::{GlobalShortcut, ShortcutManager},
-  keycode::{keycode_from_scancode, keycode_to_scancode},
-};
-use std::fmt;
-
-#[non_exhaustive]
-#[derive(Debug)]
-pub enum OsError {
-  CGError(core_graphics::base::CGError),
-  CreationError(&'static str),
-}
-
-impl fmt::Display for OsError {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self {
-      OsError::CGError(e) => f.pad(&format!("CGError {}", e)),
-      OsError::CreationError(e) => f.pad(e),
-    }
-  }
-}
+#[cfg(target_os = "macos")]
+#[path = "macos/mod.rs"]
+mod platform;
